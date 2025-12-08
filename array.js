363a2aec24ee27d10d7f5449a381489f05d1586e
rickyrian248 <rickyrian248@gmail.com>
2025-12-08 19:06:33 +0300

    modified array.js

--- array.js
+++ array.js
@@ -46,3 +46,59 @@ function teamNames() {
 
   return names;
 }
+
+["Brooklyn Nets", "Charlotte Hornets"]
+
+function playerNumbers(teamName) {
+  const game = gameObject();
+
+  for (let teamKey in game) {
+    const team = game[teamKey];
+
+    if (team.teamName === teamName) {
+      const numbers = [];
+
+      for (let player in team.players) {
+        numbers.push(team.players[player].number);
+      }
+
+      return numbers;
+    }
+  }
+}
+
+function playerStats(playerName) {
+  const game = gameObject();
+
+  for (let teamKey in game) {
+    const team = game[teamKey];
+    const players = team.players;
+
+    if (players[playerName]) {
+      return players[playerName];
+    }
+  }
+}
+
+function bigShoeRebounds() {
+  const game = gameObject();
+  let largestShoe = 0;
+  let rebounds = 0;
+
+  for (let teamKey in game) {
+    const team = game[teamKey];
+    const players = team.players;
+
+    for (let player in players) {
+      const shoe = players[player].shoe;
+
+      if (shoe > largestShoe) {
+        largestShoe = shoe;
+        rebounds = players[player].rebounds;
+      }
+    }
+  }
+
+  return rebounds;
+}
+
